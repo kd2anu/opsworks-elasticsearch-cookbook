66edad40c3d63f4a0b027588f0a8477609023a84
kd2anu <kd2anu.7@gmail.com>
2017-12-21 14:11:56 -0500

    remove -b from plugin cli

--- es/recipes/default.rb
+++ es/recipes/default.rb
@@ -39,7 +39,7 @@ end
 
 # cloud-aws plugin:
 execute 'install cloud-aws plugin' do
-  command '/usr/share/elasticsearch/bin/plugin install cloud-aws -b'
+  command '/usr/share/elasticsearch/bin/plugin install cloud-aws'
   not_if '[ -d /usr/share/elasticsearch/plugins/cloud-aws ]'
 end
 
